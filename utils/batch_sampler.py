198dca3d197721fcaa17cca4ac868d0c8fb3635a
Дарья Барановская <darya.baranovskaya01082001@gmail.com>
2021-11-27 17:36:14 +0300

    first commit

---
+++ utils/batch_sampler.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Tuple, Dict, Optional, List, Union
+from itertools import islice
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+import torch
+
+@dataclass
+class Batch:
+    waveform: torch.Tensor
+    waveforn_length: torch.Tensor
+    transcript: List[str]
+    tokens: torch.Tensor
+    token_lengths: torch.Tensor
+    durations: Optional[torch.Tensor] = None
+
+    def to(self, device: torch.device) -> 'Batch':
+        raise NotImplementedError
---
+++ utils/ljspeech_dataset.py
@@ -0,0 +1,48 @@
+import torch
+import torchaudio
+from typing import Tuple, Dict, Optional, List, Union
+from torch.nn.utils.rnn import pad_sequence
+
+class LJSpeechDataset(torchaudio.datasets.LJSPEECH):
+
+    def __init__(self, root):
+        super().__init__(root=root)
+        self._tokenizer = torchaudio.pipelines.TACOTRON2_GRIFFINLIM_CHAR_LJSPEECH.get_text_processor()
+
+    def __getitem__(self, index: int):
+        waveform, _, _, transcript = super().__getitem__(index)
+        waveforn_length = torch.tensor([waveform.shape[-1]]).int()
+
+        tokens, token_lengths = self._tokenizer(transcript)
+
+        return waveform, waveforn_length, transcript, tokens, token_lengths
+
+    def decode(self, tokens, lengths):
+        result = []
+        for tokens_, length in zip(tokens, lengths):
+            text = "".join([
+                self._tokenizer.tokens[token]
+                for token in tokens_[:length]
+            ])
+            result.append(text)
+        return result
+
+
+class LJSpeechCollator:
+
+    def __call__(self, instances: List[Tuple]) -> Dict:
+        waveform, waveforn_length, transcript, tokens, token_lengths = list(
+            zip(*instances)
+        )
+
+        waveform = pad_sequence([
+            waveform_[0] for waveform_ in waveform
+        ]).transpose(0, 1)
+        waveforn_length = torch.cat(waveforn_length)
+
+        tokens = pad_sequence([
+            tokens_[0] for tokens_ in tokens
+        ]).transpose(0, 1)
+        token_lengths = torch.cat(token_lengths)
+
+        return Batch(waveform, waveforn_length, transcript, tokens, token_lengths)
